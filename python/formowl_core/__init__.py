8ca81e085948f8459485712eced7334e5d622763
markliou <yifanliou@gmail.com>
2026-07-22 12:48:19 +0800

    POC: wire frozen Jieba SentencePiece tokenizer

--- python/formowl_core/__init__.py
+++ python/formowl_core/__init__.py
@@ -1,8 +1,22 @@
 from .core import diff_lines, sha256_prefixed, sha256_prefixed_id
 from .json_files import read_json_object, write_json_atomic
+from .tokenization import (
+    ASCII_IDENTIFIER_REGEX_TOKENIZER_ID,
+    JIEBA_SENTENCEPIECE_FROZEN_PROFILE_TOKENIZER_ID,
+    ascii_identifier_regex_tokens,
+    configured_mail_candidate_admission_tokens,
+    configured_mail_tokenizer_id,
+    jieba_sentencepiece_frozen_profile_candidate_admission_tokens,
+)
 
 __all__ = [
+    "ASCII_IDENTIFIER_REGEX_TOKENIZER_ID",
+    "JIEBA_SENTENCEPIECE_FROZEN_PROFILE_TOKENIZER_ID",
+    "ascii_identifier_regex_tokens",
+    "configured_mail_candidate_admission_tokens",
+    "configured_mail_tokenizer_id",
     "diff_lines",
+    "jieba_sentencepiece_frozen_profile_candidate_admission_tokens",
     "read_json_object",
     "sha256_prefixed",
     "sha256_prefixed_id",
